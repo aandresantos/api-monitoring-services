9b30f14c0ec267a9cdc8f7b95d41bb61b860496f
André Santos <andrelucianodossantosjunior@gmail.com>
2025-07-14 12:43:49 -0300

    feat: created fist route to add services in memory

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"sync"
+
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+type Service struct {
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	URLAddress string `json:"urlAddress"`
+	Status     string `json:"status"`
+}
+
+var serviceStore = make(map[string]Service)
+var storeMutex = &sync.Mutex{}
+
+func main() {
+	Echo := echo.New()
+
+	Echo.POST("/services", addService)
+
+	Echo.Logger.Fatal(Echo.Start(":3000"))
+}
+
+func addService(ctx echo.Context) error {
+	var serv Service
+
+	if err := ctx.Bind(&serv); err != nil {
+		return ctx.String(http.StatusBadRequest, "Erro ao adicionar Serviço")
+	}
+
+	serv.ID = uuid.New().String()
+	serv.Status = "Pending"
+
+	storeMutex.Lock()
+	serviceStore[serv.ID] = serv
+	storeMutex.Unlock()
+
+	return ctx.JSON(http.StatusCreated, serv)
+}
