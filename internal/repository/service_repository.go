5f392f71e9f090793e56e0ce921dfebf517d77b0
André Santos <andrelucianodossantosjunior@gmail.com>
2025-07-15 23:51:22 -0300

    feat: created the first method for repository layer

---
+++ internal/repository/service_repository.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"api-monitoring-services/internal/domain"
+	"sync"
+)
+
+type ServiceRepository interface {
+	GetAll() []domain.Service
+}
+
+type InMemoryServiceRepository struct {
+	store map[string]*domain.Service
+	mutex *sync.RWMutex
+}
+
+func NewInMemoryServiceRepository() *InMemoryServiceRepository {
+
+	return &InMemoryServiceRepository{
+		store: make(map[string]*domain.Service),
+		mutex: &sync.RWMutex{},
+	}
+}
+
+func (r *InMemoryServiceRepository) GetAll() []domain.Service {
+	r.mutex.RLock()
+	defer r.mutex.Unlock()
+
+	services := make([]domain.Service, 0, len(r.store))
+
+	for _, service := range r.store {
+		services = append(services, *service)
+	}
+
+	return services
+}
