b7bc505ce9746b5b33fe0a8b88c301bf381dafda
André Santos <andrelucianodossantosjunior@gmail.com>
2025-07-15 23:23:47 -0300

    chore: created the first layer to rewrite code using Layered Arch

---
+++ internal/domain/service.go
@@ -0,0 +1,22 @@
+package domain
+
+import (
+	"time"
+)
+
+type ServiceStatus string
+
+const (
+	StatusPeding  ServiceStatus = "pending"
+	StatusOnline  ServiceStatus = "online"
+	StatusOffline ServiceStatus = "offline"
+)
+
+type Service struct {
+	ID         string        `json:"id"`
+	Name       string        `json:"name"`
+	URLAddress string        `json:"urlAddress"`
+	Status     ServiceStatus `json:"status"`
+	CreatedAt  time.Time     `json:"createdAt"`
+	UpdatedAt  time.Time     `json:"updatedAt"`
+}
